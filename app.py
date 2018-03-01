cf2d7f38a7d3b862cd77255c6fc67616d4ab385c
Amelia <amy@Amelias-MacBook-Pro.local>
2018-03-01 15:05:07 -0500

    added Locations module

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-def lookup_by_location():
+
+from locations import Location
+
+def lookup_by_location(location):
     pass
 
 def sh_generator():
---
+++ locations.py
@@ -0,0 +1,12 @@
+class Location():
+    def __init__(self):
+        pass
+
+    def as_mgrs(self):
+        pass
+    def as_latlon(self):
+        pass
+    def as_utm(self):
+        pass
+    def as_ne(self):
+        pass
