08576bda66f1a5151e81069a4dd6c39d9f073c9f
Amelia <apottr@akira.local>
2018-03-03 00:29:20 -0500

    wrote sqlite commands for adding source, looking up, etc

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
+import sqlite3
 
-from locations import Location
+conn = sqlite3.connect('sources.db')
 
-def lookup_by_location(location):
-    pass
+def add_source(src):
+    c = conn.cursor()
+    try:
+        c.execute('insert into sources values (?,?,?)',(src["name"],src["command"],src["loctag"]))
+        return True
+    except sqlite3.DatabaseError as e:
+        print(e)
+        return False
 
-def sh_generator():
-    pass
+def lookup_by_tag(tag):
+    c = conn.cursor()
+    c.execute('select * from sources where loctag like "%?%"',(tag,))
+    return c.fetchall()
+
+def sh_generator(tag):
+    lines = ["#!/bin/sh"]
+    f = lookup_by_tag(tag)
+    if len(f) != 0:
+        for entry in f:
+            lines.append("{} #{}".format(entry[1],entry[0]))
+    return lines
 
 def add_sh_to_cron(sh):
-    pass
+    out = "\n".join(sh)
+    
