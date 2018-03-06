f06492f74a913eabdfe98887db4b9db6ecce97cf
Amelia <amy@Amelias-MacBook-Pro.local>
2018-03-06 18:41:30 -0500

    added html pages, added light functionality, added source adding

--- app.py
+++ app.py
@@ -1,26 +1,90 @@
 import sqlite3,os
 from hashlib import sha1
 from pathlib import Path
-
 from crontab import CronTab
+from flask import Flask,request,render_template,redirect
 
-conn = sqlite3.connect('sources.db')
+dbname = 'sources.db'
 cron = CronTab(user=True)
 directory = Path(__file__).parent
+app = Flask(__name__)
 
-def add_source(src):
+def init_db():
+    conn = sqlite3.connect(dbname)
     c = conn.cursor()
     try:
-        c.execute('insert into sources values (?,?,?)',(src["name"],src["command"],src["loctag"]))
+        c.execute('select * from sources')
+    except sqlite3.OperationalError:
+        c.execute('create table sources (name text, command text, loctag text)')
+    try:
+        c.execute('select * from jobs')
+    except sqlite3.OperationalError:
+        c.execute('create table jobs (name text, tags text)')
+
+    c.close()
+
+def add_source(src):
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    try:
+        c.execute('insert into sources values (?,?,?)',(src["name"],src["command"],src["tag"]))
+        conn.commit()
+        conn.close()
         return True
-    except sqlite3.DatabaseError as e:
+    except Exception as e:
         print(e)
         return False
 
 def lookup_by_tag(tag):
+    conn = sqlite3.connect(dbname)
     c = conn.cursor()
-    c.execute('select * from sources where loctag like "%?%"',(tag,))
+    c.execute('select * from sources where loctag like "%?,%"',(tag,))
     return c.fetchall()
+
+def get_tags():
+    out = {}
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    c.execute("select loctag from sources")
+    r = c.fetchall()
+    if len(r) != 0:
+        for item in r:
+            x = item[0].split(",")
+            for tag in x:
+                if tag in out:
+                    out[tag] += 1
+                else:
+                    out[tag] = 1
+    return out
+
+def get_sources():
+    out = []
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    c.execute("select * from sources")
+    r = c.fetchall()
+    if len(r) != 0:
+        for item in r:
+            out.append({
+                "name": item[0],
+                "snippet": item[1],
+                "tags": item[2]
+            })
+    return out
+
+def get_jobs():
+    out = []
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    c.execute("select * from jobs")
+    r = c.fetchall()
+    if len(r) != 0:
+        for item in r:
+            out.append({
+                "name": item[0],
+                "tags": item[1]
+            })
+    return out
 
 def sh_generator(tag):
     lines = ["#!/bin/sh"]
@@ -44,3 +108,37 @@ def add_sh_to_cron(name,sh,schedule):
     else:
         os.remove(fname)
         return False
+
+@app.route("/")
+def index_route():
+    return render_template("index.html",sources=get_sources(),jobs=get_jobs())
+
+@app.route("/css")
+def css_route():
+    return """
+        input[name="command"], .snippet{
+            font-family: monospace;
+        }
+        ul {
+            list-style-type: none;
+        }
+    """
+
+@app.route("/add_source", methods=["GET","POST"])
+def add_source_route():
+    if request.method == "GET":
+        return render_template("add_source.html",tags=get_tags())
+    else:
+        f = request.form
+        print(f)
+        add_source(f)
+        return redirect("/")
+
+@app.route("/add_job", methods=["GET","POST"])
+def add_job_route():
+    if request.method == "GET":
+        return render_template("add_job.html")
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
