c44f9f7ca4ab18c87e5ff99ff73ba45d401c8698
Amelia <apottr@akira.local>
2018-05-27 00:42:28 -0400

    started rewrite

--- helper_functions/darwin_mount_handler.py
+++
@@ -1,8 +0,0 @@
-class mnt:
-    def Table(self):
-        pass
-    def Tab(self):
-        pass
-    def Context(self):
-        pass
-    
--- helper_functions/globals.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-directory = Path(__file__).parent.parent.resolve() #pylint: disable=no-member
-sources_db = str(directory / "databases" / "sources.db")
-projects_db = str(directory / "databases" / "projects.db")
-rem_port = 18861
