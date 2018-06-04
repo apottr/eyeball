386f35dd290b12eba64a0cbd0ed8ce3b370f47b0
Amelia <apottr@akira.local>
2018-06-04 01:10:21 -0400

    wrote code

--- es_ops.py
+++ es_ops.py
@@ -15,8 +15,13 @@ def guarantee_index_exists(idx):
     if "error" in d:
         requests.put(esurl(f"/{idx}"))
 
-def create_job():
-    pass
+def create_obj(p,obj):
+    r = requests.post(esurl(p),data=obj,headers={"Content-Type":"application/json"})
+    return r
+
+def create_job(f):
+    obj = f
+    create_obj("/jobs/job",obj)
 
 def get_jobs():
     r = requests.get(esurl("/jobs/_search"))
@@ -26,8 +31,9 @@ def get_sources():
     r = requests.get(esurl("/sources/_search"))
     return r.json()
 
-def create_source():
-    pass
+def create_source(f):
+    obj = f
+    create_obj("/sources/source",obj)
 
 def db_init():
     for idx in ["jobs","sources"]:
